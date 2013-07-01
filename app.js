2b5afcd9d1d8e1b21cfa718530a99b3733268b0e
Blaz Snuderl <snuderl@gmail.com>
2013-07-01 14:26:16 +0200

    Made work on Heroku

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var server=http.createServer(function  (request, response) {
 console.log("Serving on port:"+process.env.PORT);
 
 var clients = {};
+
 var games = {};
 var besede = fs.readFileSync('besede.txt').toString().split("\n");
 
